c5f0befef3b8ff0dc894850a727fcffc5fe2f1e0
novichkovv <novichkovv@bk.ru>
2015-09-19 20:08:51 +0300

    r
    Signed-off-by: novichkovv <novichkovv@bk.ru>

---
+++ www/controllers/frontend/common_controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus1
+ * Date: 19.09.2015
+ * Time: 21:08
+ */
+class common_controller extends controller
+{
+    public function index()
+    {
+        
+    }
+}
