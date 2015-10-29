9bc7a67cb5c9d8b5e92c2376fddb95bb811bc6e9
Novichkov <novichvkovv@bk.ru>
2015-10-29 12:10:06 +0300

    No Message
    Commit with no message
    
    Signed-off-by: Novichkov <novichvkovv@bk.ru>

---
+++ www/mail.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enovichkov
+ * Date: 29.10.2015
+ * Time: 11:21
+ */
+$result = mail('yourmail@domain.ru', 'subject', 'message');
+
+if($result)
+{
+    echo 'все путем';
+}
+else
+{
+    echo 'что-то не так';
+}
